d7a4f8e6a2f380b0807e718bd524db71f55b9385
MrCastro04 <slavamora684@gmail.com>
2025-07-17 21:17:18 +0300

    жжжжжж

--- Balloon Game/Assets/Modules/new/AvatarButton.cs
+++ Balloon Game/Assets/Modules/new/AvatarButton.cs
@@ -6,12 +6,15 @@ public class AvatarButton : BaseButton
 
     protected override void OnClickAction()
     {
-        Debug.Log("Click");
         if (_myProfileScreen.AvailableAvatars.Count == 0) return;
 
         Sprite randomAvatar = _myProfileScreen.AvailableAvatars[Random.Range(0, _myProfileScreen.AvailableAvatars.Count)];
         _myProfileScreen.AvatarImage.sprite = randomAvatar;
+        
+        if (_myProfileScreen.AvatarImage != null)
+            _myProfileScreen.AvatarImage.raycastTarget = true;
 
         SaveSystem.Instance.SavePlayerAvatarPath(randomAvatar.name);
     }
+
 }
