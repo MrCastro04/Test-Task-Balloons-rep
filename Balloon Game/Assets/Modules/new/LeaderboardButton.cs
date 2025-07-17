46f86c21f9d8e04f5fd207f83443959c9608eb9a
MrCastro04 <slavamora684@gmail.com>
2025-07-18 00:16:49 +0300

    доделал лидер борд

---
+++ Balloon Game/Assets/Modules/new/LeaderboardButton.cs
@@ -0,0 +1,13 @@
+﻿using Modules.Core.Game_Actions;
+using Modules.Core.Systems.Action_System.Scripts;
+using UnityEngine;
+
+public class LeaderboardButton : BaseButton
+{
+    [SerializeField] private LeaderBoardScreen _leaderBoardScreen;
+
+    protected override void OnClickAction()
+    {
+        ActionSystem.Instance.Perform(new OpenScreenGA(_leaderBoardScreen));
+    }
+}
